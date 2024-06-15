b56012bf825cefd4c8f8b75df11febf572dad8d4
Ariel Nunes da Silva <arielnds274@gmail.com>
2024-06-15 11:35:40 -0300

    test

--- pages/script.js
+++ pages/script.js
@@ -1,5 +1,9 @@
 function Home(){
-    return <span></span>
+    <body>
+        <h1>Vai perder 10 seguida no fifa maninho</h1> 
+        <img src="https://i.ytimg.com/vi/4Ylmo83Ajf8/maxresdefault.jpg"></img>
+        <span> puts.. vc desistiu mesmo kk</span>
+    </body>
 }
 
 export default Home;
