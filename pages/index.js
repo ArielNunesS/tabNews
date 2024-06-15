06c01eb57c17850688b18f25002bf84f7d12c0df
Ariel Nunes da Silva <arielnds274@gmail.com>
2024-06-15 11:40:18 -0300

    fix: fixing index.js file

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+        return <div> <h1>Vai perder 10 seguida no fifa maninho</h1><img src="https://i.ytimg.com/vi/4Ylmo83Ajf8/maxresdefault.jpg"></img></div>  
+}
+
+export default Home;
